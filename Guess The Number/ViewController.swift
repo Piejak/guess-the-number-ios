fd0dfeedcdbf01ca323feee7d5003f352863104b
Brian Piejak <Brian.Piejak@gmail.com>
2017-01-29 21:03:25 -0500

    Layout basic app functionality

--- Guess The Number/ViewController.swift
+++ Guess The Number/ViewController.swift
@@ -10,9 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBAction func submitButtonPressed(_ sender: UIButton) {
+        if let guess = guessTextField.text {
+            if let guessInt = Int(guess) {
+                numberOfGuesses += 1
+                validateGuess(guessInt)
+            }
+        }
+    }
+    
+    let lowerBound = 1
+    let upperBound = 100
+    var numberToGuess: Int!
+    var numberOfGuesses = 0
+    
+    func generateRandomNumber() {
+        numberToGuess = Int(arc4random_uniform(100)) + 1
+    }
+    
+    func validateGuess(_ guess:Int) {
+        if guess < lowerBound || guess > upperBound {
+            showBoundsAlert()
+        } else if guess < numberToGuess {
+            guessLabel.text = "Guess higher ⬆️"
+        } else if guess > numberToGuess {
+            guessLabel.text = "Guess lower ⬇️"
+        } else {
+            showWinsAlert()
+            numberOfGuesses = 0
+            guessLabel.text = "Guess The Number"
+            generateRandomNumber()
+        }
+        guessTextField.text = ""
+    }
+    
+    func showBoundsAlert() {
+        let alert = UIAlertController(title: "Hey!",
+                                      message: "You should guess between 1 and 100",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Got it",
+                                   style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showWinsAlert() {
+        let alert = UIAlertController(title: "Nailed it 🎉",
+                                      message: "You used \(numberOfGuesses) guesses to win", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Play again",
+                                   style: .default,
+                                   handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        generateRandomNumber()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, ’10.0’
+
+target 'Guess The Number' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+
+  # Pods for Guess The Number
+
+  target 'Guess The NumberTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
